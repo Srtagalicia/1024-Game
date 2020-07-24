49f3362a7c099df7acd8c615d44dad343c0e5fc2
srtagalicia <srtagalicia@gmail.com>
2020-07-24 11:55:12 +0200

    addCell function fixed

--- game.js
+++ game.js
@@ -1,22 +1,40 @@
 $(document).ready(function start() {
     bindKeys();
-    addCell();
+    //addCell();
+    addRandomValues()
+    addRandomValues();
     drawBoard();
 });
 
 function drawBoard() {
     for (let r = 0; r < 3; r++) {
-    for (let c = 0; c < 3; c++) {
-        let selector = `[data-row='${r}'][data-col='${c}']`;
-        if (board[r][c] === 0){
-            $(selector).attr('class', 'cell').text('');
-        }
-        else {
-            $(selector).attr('class', `cell cell${board[r][c]}`).text(board[r][c]);
-        }  
-          }
+        for (let c = 0; c < 3; c++) {
+            let selector = `[data-row='${r}'][data-col='${c}']`;
+            if (board[r][c] === 0){
+                $(selector).attr('class', 'cell').text('');
+            }
+            else {
+                $(selector).attr('class', `cell cell${board[r][c]}`).text(board[r][c]);
+            }  
         }
     }
+    console.log(board)
+}
+
+function getRandomIndex(myArray){
+   return Math.floor(Math.random() * myArray.length)
+}
+
+function addRandomValues(){
+    let row = getRandomIndex(board);
+    let col = getRandomIndex(board);
+    if (board[row][col] == 0){
+        board[row][col] = 2 
+    }
+    else {
+        addRandomValues();
+    }
+}
 
 function bindKeys() {
     $('body').on('keydown', function (e){
@@ -24,18 +42,22 @@ function bindKeys() {
             case 38:
                 moveUp();
                 endMove();
+                addRandomValues();
                 break;
             case 40:
                 moveDown();
                 endMove();
+                addRandomValues();
                 break;
             case 37:
                 moveLeft();
                 endMove();
+                addRandomValues();
                 break;
             case 39:
                 moveRight();
                 endMove();
+                addRandomValues();
                 break;
             case 82:
                 initialiseGame();
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let board = [
-    [0, 0, 0], 
-    [0, 0, 0], 
-    [0, 0, 0], 
+  [0, 0, 0], 
+  [0, 0, 0], 
+  [0, 0, 0], 
 ];
 
 let temporaryBoard = [];
@@ -9,139 +9,149 @@ let emptyCells = [];
 let gameOver = false;
 
 function initialiseGame() {  
-    board = [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]
-    ];
-    temporaryBoard = [];
-    gameOver = false;
-    readEmptyCells();
-    addCell();
-    readEmptyCells();
-    addCell();
+  board = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0]
+  ];
+  temporaryBoard = [];
+  gameOver = false;
+  readEmptyCells();
+  addCell();
+  readEmptyCells();
+  addCell();
 }
 
 function readEmptyCells() {
-    emptyCells = [];
-    for (let r = 0; r < 3; r++) {
-    for (let c = 0; c < 3; c++) {
-      if (board[r][c] === 0) {
-        emptyCells.push([r, c]);
-      }
+  emptyCells = [];
+  for (let r = 0; r < 3; r++) {
+  for (let c = 0; c < 3; c++) {
+    if (board[r][c] === 0) {
+      emptyCells.push([r, c]);
     }
   }
-  if (emptyCells.length === 0) {
-    gameOver = true;
-  }
+}
+if (emptyCells.length === 0) {
+  gameOver = true;
+}
 }
 
 function selectRadomValueFromArray(myArray) {
-    let randomIndex = Math.floor(Math.random() * myArray.length);
-    return myArray[randomIndex];
+  let randomIndex = Math.floor(Math.random() * myArray.length);
+  return myArray[randomIndex];
 }
 
 function addCell() {
-    let cellCoordinates = selectRadomValueFromArray(emptyCells);
-    let r = cellCoordinates[0];
-    let c = cellCoordinates[1];
-    board[r][c] = selectRadomValueFromArray([2, 4]);
+  let cellCoordinates = selectRadomValueFromArray(emptyCells);
+  let r = cellCoordinates[0];
+  let c = cellCoordinates[1];
+  board[r][c] = selectRadomValueFromArray([2, 4]);
 }
 
-function rotateMatrix(numberofRotations, finalRotation) {
-    let tempMatrix = [];
-    if (finalRotation) {
-      tempMatrix = [...temporaryBoard];
-    } 
-    else {
-        tempMatrix = [...board];
-    }
-    let n = tempMatrix.length;
-    for (let rotation = 0; rotation < numberOfRotations; rotation++) {
-      for (let i = 0; i < n / 2; i++) {
-        for (let j = i; j < n - i - 1; j++) {
-          let tmp = tempMatrix[i][j];
-          tempMatrix[i][j] = tempMatrix[n - j - 1][i];
-          tempMatrix[n - j - 1][i] = tempMatrix[n - i - 1][n - j - 1];
-          tempMatrix[n - i - 1][n - j - 1] = tempMatrix[j][n - i - 1];
-          tempMatrix[j][n - i - 1] = tmp;
-        }
+function rotateMatrix(numberOfRotations, finalRotation) {
+  let tempMatrix = [];
+  if (finalRotation) {
+    tempMatrix = [...temporaryBoard];
+  } 
+  else {
+      tempMatrix = [...board];
+  }
+  let n = tempMatrix.length;
+  for (let rotation = 0; rotation < numberOfRotations; rotation++) {
+    for (let i = 0; i < n / 2; i++) {
+      for (let j = i; j < n - i - 1; j++) {
+        let tmp = tempMatrix[i][j];
+        tempMatrix[i][j] = tempMatrix[n - j - 1][i];
+        tempMatrix[n - j - 1][i] = tempMatrix[n - i - 1][n - j - 1];
+        tempMatrix[n - i - 1][n - j - 1] = tempMatrix[j][n - i - 1];
+        tempMatrix[j][n - i - 1] = tmp;
       }
     }
-    if (finalRotation) {
-      board = [...tempMatrix];
-    } 
-    else {
-      temporaryBoard = [...tempMatrix];
-    }
-}
-function moveColumn(a, b, c) {
-    if (b === c) {
-      c = c * 2;
-      b = 0
-    } 
-    else if (b !== 0 && c == 0) {
-      c = b;
-      b = 0;
-    }
+  }
   
-    if (a === b) {
-      b = b * 2;
+  if (finalRotation) {
+    console.log('Temp matrix ', tempMatrix)
+    board = [...tempMatrix];
+  } 
+  else {
+    temporaryBoard = [...tempMatrix];
+  }
+}
+
+function moveColumn(a, b, c) {
+  if (b === c) {
+    c = c * 2;
+    b = 0
+  } 
+  else if (b !== 0 && c == 0) {
+    c = b;
+    b = 0;
+  }
+
+  if (a === b) {
+    b = b * 2;
+    a = 0;
+  } 
+  else if (a !== 0 && b === 0) {
+    if (c === 0) {
+      c = a;
       a = 0;
     } 
-    else if (a !== 0 && b === 0) {
-      if (c === 0) {
-        c = a;
-        a = 0;
-      } 
-      else if (c === a) {
-        c = c * 2;
-        a = 0;
-      } 
-      else {
-        b = a;
-        a = 0;
-      }
+    else if (c === a) {
+      c = c * 2;
+      a = 0;
+    } 
+    else {
+      b = a;
+      a = 0;
     }
-    return [a, b, c];
   }
+  return [a, b, c];
+}
 
-  function move() {
-    for (let i = 0; i < 3; i++) {
-      let ordered = moveColumn(temporaryBoard[0][i], temporaryBoard[1][i], temporaryBoard[2][i]);
-      temporaryBoard[0][i] = ordered[0];
-      temporaryBoard[1][i] = ordered[1];
-      temporaryBoard[2][i] = ordered[2];
-    }
+
+
+
+
+
+
+
+function move() {
+  for (let i = 0; i < 3; i++) {
+    let ordered = moveColumn(temporaryBoard[0][i], temporaryBoard[1][i], temporaryBoard[2][i]);
+    temporaryBoard[0][i] = ordered[0];
+    temporaryBoard[1][i] = ordered[1];
+    temporaryBoard[2][i] = ordered[2];
   }
-  
-  function moveUp() {
-    rotateMatrix(2, false); 
-    move(); 
-    rotateMatrix(2, true); 
-  }
-  
-  function moveRight() {
-    rotateMatrix(1, false);  
-    move(); 
-    rotateMatrix(3, true);
-  }
-  
-  function moveDown() {
-    temporaryBoard = [...board]
-    console.log(board, temporaryBoard);
-    move();
-    board = [...temporaryBoard]
-  }
-  
-  function moveLeft() {
-    rotateMatrix(3, false); 
-    move(); 
-    rotateMatrix(1, true);
-  }
-  
-  function endMove() {
-    readEmptyCells();
-    addCell();
-    drawBoard();
-  }
+}
+
+function moveUp() {
+  rotateMatrix(2, false); 
+  move(); 
+  rotateMatrix(2, true); 
+}
+
+function moveRight() {
+  rotateMatrix(1, false);  
+  move(); 
+  rotateMatrix(3, true);
+}
+
+function moveDown() {
+  temporaryBoard = [...board]
+  console.log(board, temporaryBoard);
+  move();
+  board = [...temporaryBoard]
+}
+
+function moveLeft() {
+  rotateMatrix(3, false); 
+  move(); 
+  rotateMatrix(1, true);
+}
+
+function endMove() {
+  readEmptyCells();
+ // addCell();
+  drawBoard();
+}
